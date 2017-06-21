e834e87661528b1c4b45a689030c5044b0e0a456
hennip <jeannie.jne@gmail.com>
2017-06-21 10:12:24 +0300

    tidy work in process

--- tidy-smolts-data.R
+++ tidy-smolts-data.R
@@ -5,11 +5,18 @@
 library(tidyverse)
 
 # Smolts
-#S<-read.table(paste(sep="",pathIn,"Smolts_6years.txt"),header=T)
-S<-read.table(paste(sep="",pathIn,"Smolts_6years_2008weekendsOff.txt"),header=T)
-S2<-cbind(S, 1:61)
+############
+#S<-read.table(paste(sep="",pathIn,"Smolts_6years_2008weekendsOff.txt"),header=T)
+#S<-read_tsv(paste(sep="",pathIn,"Smolts_6years.txt"))
+S<-read_tsv(paste(sep="",pathIn,"Smolts_6years_2008weekendsOff.txt"))
+# Leave S as such, goes to JAGS model as input
+day<-tibble(Day=1:61)
+S2<-bind_cols(S, day)
+rename(S2, N03=2003)
+
+,N04=`2004`,N05=`2005`,N06=`2006`,N08=`2008`,N14=`2014`)
+
 colnames(S2)<-c(2003:2006,2008,2014, "day")
-(S2<-as.tibble(S2))
 
 # re-arrange  
 ts1<- S2%>%
@@ -18,7 +25,8 @@ ts1<- S2%>%
 ts1<-select(ts1, year, everything()) 
 
 # Temperature
-temp1<-read.table(paste(sep="",pathIn,"Temperature_4years.txt"),header=T)
+#temp1<-read.table(paste(sep="",pathIn,"Temperature_4years.txt"),header=T)
+temp1<-read_tsv(paste(sep="",pathIn,"Temperature_4years.txt"))
 source("data-water-temperature.r")
 datT<-cbind(temp1[,1:2],round(DailyTemp,1), temp1[,3],temp1[,4],round(DailyTemp14,1))
 temp2<-cbind(datT,1:61)
