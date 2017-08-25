d64d6a0d0bf8c1c17062ba3b99119367a13a2c0a
hennip <jeannie.jne@gmail.com>
2017-08-25 12:23:34 +0300

    minor changes

--- load-simulations.R
+++ load-simulations.R
@@ -4,7 +4,8 @@
 #load(file=paste(sep="",pathOut,"Smolts_17_04_26.RData")) # vaikuttaa lupaavalta, konvergenssi 1vrk jälkeen?
 #load(file=paste(sep="",pathOut,"Smolts_17_06_21.RData")) #
 #load(file=paste(sep="",pathOut,"Smolts_cvDs_17_08_11.RData")) # cvmuD & cvD max 2
-load(file=paste(sep="",pathOut,"Smolts_aDbD_17_08_14.RData")) # cvmuD & cvD max 2; aD & bD vastaa Panun näkemystä 
+#load(file=paste(sep="",pathOut,"Smolts_aDbD_17_08_14.RData")) # cvmuD & cvD max 2; aD & bD vastaa Panun näkemystä 
+load(file=paste(sep="",pathOut,"Smolts_aDbD_17_08_30.RData")) # cvmuD max 1, aD priori korjattu
 
 chains<-window(chains,start=50000, thin=200)
 
