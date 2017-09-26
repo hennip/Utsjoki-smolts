c298887235652de8511d19a56d05dd47c214d692
hennip <jeannie.jne@gmail.com>
2017-09-26 13:35:59 +0300

    run setup

--- 03-Model/arrival-model.R
+++ 03-Model/arrival-model.R
@@ -195,12 +195,12 @@ system.time(chains0<-coda.samples(jm,variable.names=var_names,n.iter=100, thin=1
  
 a1<-Sys.time();a1
 system.time(
-  chains1<-coda.samples(jm,variable.names=var_names,n.iter=1000, thin=200))/3600
+  chains1<-coda.samples(jm,variable.names=var_names,n.iter=300000, thin=200))/3600
 b1<-Sys.time() ; t1<-b1-a1; t1
 
 a2<-Sys.time()
 system.time(
-  chains2<-coda.samples(jm,variable.names=var_names,n.iter=1000, thin=200))/3600
+  chains2<-coda.samples(jm,variable.names=var_names,n.iter=300000, thin=200))/3600
 b2<-Sys.time() ; t2<-b2-a2; t2
 
 chains<-combine.mcmc(list(chains1, chains2))
@@ -208,7 +208,7 @@ save(chains, file=str_c(pathOut,modelName,".RData"))
 
 a3<-Sys.time()
 system.time(
-  chains3<-coda.samples(jm,variable.names=var_names,n.iter=100000, thin=200))/3600
+  chains3<-coda.samples(jm,variable.names=var_names,n.iter=300000, thin=200))/3600
 b3<-Sys.time() ; t3<-b3-a3; t3
 
 chains<-combine.mcmc(list(chains2, chains3))
@@ -217,7 +217,7 @@ save(chains, file=str_c(pathOut,modelName,".RData"))
 a4<-Sys.time()
 Sys.time()
 system.time(
-  chains4<-coda.samples(jm,variable.names=var_names,n.iter=100000, thin=200))/3600
+  chains4<-coda.samples(jm,variable.names=var_names,n.iter=300000, thin=200))/3600
 b4<-Sys.time() ; t4<-b4-a4; t4
 
 chains<-combine.mcmc(list(chains2, chains3, chains4))
@@ -226,10 +226,17 @@ save(chains, file=str_c(pathOut,modelName,".RData"))
 a5<-Sys.time()
 Sys.time()
 system.time(
-  chains5<-coda.samples(jm,variable.names=var_names,n.iter=400000, thin=200))/3600
+  chains5<-coda.samples(jm,variable.names=var_names,n.iter=300000, thin=200))/3600
 b5<-Sys.time() ; t5<-b5-a5; t5
 
 chains<-combine.mcmc(list(chains2, chains3, chains4,chains5))
 save(chains, file=str_c(pathOut,modelName,".RData"))
 
+a6<-Sys.time()
+Sys.time()
+system.time(
+  chains6<-coda.samples(jm,variable.names=var_names,n.iter=300000, thin=200))/3600
+b6<-Sys.time() ; t6<-b6-a6; t6
 
+chains<-combine.mcmc(list(chains2,chains3,chains4,chains5,chains6))
+save(chains, file=str_c(pathOut,modelName,".RData"))
