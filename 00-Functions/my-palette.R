0a56ae766acce0183bd41de7c5b7175351f3ccc5
hennip <jeannie.jne@gmail.com>
2017-09-27 12:31:39 +0300

    fix palette

--- 00-Functions/my-palette.R
+++ 00-Functions/my-palette.R
@@ -3,23 +3,23 @@ library(wesanderson)
 library(ghibli)
 
 # see palette names and colours
-par(mfrow=c(7,3))
-for(i in names(ghibli_palettes)) print(ghibli_palette(i))
+#par(mfrow=c(7,3))
+#for(i in names(ghibli_palettes)) print(ghibli_palette(i))
 
 #my_palette = c(brewer.pal(5, "Set1")[c(1,3,4,5)], brewer.pal(5, "Pastel1")[c(2,5,1,3)])
 
-par(mfrow=c(3,2))
-display.brewer.pal(n=8,"Dark2")
-display.brewer.pal(n=8,"Set2")
-display.brewer.pal(n=8,"Set1")
-display.brewer.pal(n=10,"Paired")
-display.brewer.pal(n=8,"RdBu")
+#par(mfrow=c(3,2))
+#display.brewer.pal(n=8,"Dark2")
+#display.brewer.pal(n=8,"Set2")
+#display.brewer.pal(n=8,"Set1")
+#display.brewer.pal(n=10,"Paired")
+#display.brewer.pal(n=8,"RdBu")
 
 #my_palette = c(
 #  wes.palette(n=5,"Darjeeling"))
 
 my_palette = c(
-  ghibli_palette("PonyoMedium")[c(3,4,1,5,6,2,8)])
+  ghibli_palette("PonyoMedium")[c(3,4,1,5,6,2,7)])
   #  brewer.pal(8, "Set1"))
   
 #my_palette = c(
