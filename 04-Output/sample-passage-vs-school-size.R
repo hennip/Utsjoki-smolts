4deb2a51f14ba5273c756f5dd57dcf740d7aecb2
hennip <jeannie.jne@gmail.com>
2018-06-21 15:48:38 +0300

    remove extra hier layer from passage vs school size plotting

--- 04-Output/sample-passage-vs-school-size.R
+++ 04-Output/sample-passage-vs-school-size.R
@@ -11,38 +11,40 @@ c1<-chains[[1]]
 #c2<-chains[[2]]
 c2<-chainsP[[1]]
 
+
 # Posterior
 
 n_samp<-length(c1[,"slope"])
 slope_samp<-c1[,"slope"]
 K_samp<-c1[,"K"]
-cvmuS_samp<-c1[,"cvmuS"]
+cvS_samp<-c1[,"cvS"]
 
-mumuS_samp<-array(NA, dim=c(n_samp,n))
+S_samp<-array(NA, dim=c(n_samp,n))
 muS_samp<-array(NA, dim=c(n_samp,n))
-SDmuS<-array(NA, dim=c(n_samp))
+SDS<-array(NA, dim=c(n_samp))
 for(j in 1:n_samp){
   for(i in 1:n){
-    SDmuS[j]<-sqrt(log(cvmuS_samp[j]^2+1))
-    mumuS_samp[j,i]<-(K_samp[j]*Ntrue[i])/((K_samp[j]/slope_samp[j])+Ntrue[i])
-    muS_samp[j,i]<-rlnorm(1,log(mumuS_samp[j,i])-0.5*SDmuS[j]*SDmuS[j], SDmuS[j])
+    SDS[j]<-sqrt(log(cvS_samp[j]^2+1))
+    muS_samp[j,i]<-(K_samp[j]*Ntrue[i])/((K_samp[j]/slope_samp[j])+Ntrue[i])
+    S_samp[j,i]<-rlnorm(1,log(muS_samp[j,i])-0.5*SDS[j]*SDS[j], SDS[j])
   }
 }
 
 # Prior
-
 n_samp<-length(c2[,"slope"])
-slope_samp<-c2[,"slope"]
-K_samp<-c2[,"K"]
-cvmuS_samp<-c2[,"cvmuS"]
+slope_sampP<-c2[,"slope"]
+K_sampP<-c2[,"K"]
+cvS_sampP<-c2[,"cvS"]
 
-mumuS_samp<-array(NA, dim=c(n_samp,n))
-muS_sampP<-array(NA, dim=c(n_samp,n))
-SDmuS<-array(NA, dim=c(n_samp))
+S_samp<-array(NA, dim=c(n_samp,n))
+muS_samp<-array(NA, dim=c(n_samp,n))
+SDS<-array(NA, dim=c(n_samp))
 for(j in 1:n_samp){
   for(i in 1:n){
-    SDmuS[j]<-sqrt(log(cvmuS_samp[j]^2+1))
-    mumuS_samp[j,i]<-(K_samp[j]*Ntrue[i])/((K_samp[j]/slope_samp[j])+Ntrue[i])
-    muS_sampP[j,i]<-rlnorm(1,log(mumuS_samp[j,i])-0.5*SDmuS[j]*SDmuS[j], SDmuS[j])
+    SDS[j]<-sqrt(log(cvS_sampP[j]^2+1))
+    muS_samp[j,i]<-(K_sampP[j]*Ntrue[i])/((K_sampP[j]/slope_sampP[j])+Ntrue[i])
+    S_sampP[j,i]<-rlnorm(1,log(muS_samp[j,i])-0.5*SDS[j]*SDS[j], SDS[j])
   }
 }
+
+
