cf8f69f456b8f1b16782dedcece28727f06085a6
hennip <jeannie.jne@gmail.com>
2018-11-29 09:45:31 +0200

    minor changes

--- 04-Output/save-stats.R
+++ 04-Output/save-stats.R
@@ -3,7 +3,7 @@ d<-as.matrix(chains)
 dim(d)
 #[1]  13334   942 # dimensions: iterations x number of variables
 
-headtext<-c("Varname","mean","sd","cv","5%","50%","95%","psrf")
+headtext<-c("Varname","mean","sd","cv","5%","50%","95%","grdPE", "grdUCI")
 statsfile<-str_c(pathOut,"stats_",ModelName,".csv")
 
 write.table(t(as.matrix(headtext)),file=statsfile,sep=',',row.names=F, col.names=F)
@@ -15,8 +15,9 @@ for(i in 1:dim(d)[2]){ # loop over all monitored variables
   q5<-quantile(d[,i],0.05)
   q50<-quantile(d[,i],0.50)
   q95<-quantile(d[,i],0.95)
-  grd<-gelman.diag(chains[,i])$psrf[1]
+  grdPE<-gelman.diag(chains[,i])$psrf[1]
+  grdUCI<-gelman.diag(chains[,i])$psrf[2]
   
-  printtxt<-c(colnames(d)[i],m,s,cv,q5,q50,q95,grd)
+  printtxt<-c(colnames(d)[i],m,s,cv,q5,q50,q95,grdPE, grdUCI)
   write.table(t(as.matrix(printtxt)),statsfile,sep=",",row.names=F, col.names=F,append=T)
 }
